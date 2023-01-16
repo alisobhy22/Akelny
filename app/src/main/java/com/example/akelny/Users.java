76a857efdc27f3cd374c9593213c12dbeba55f05
alisobhy22 <64151548+alisobhy22@users.noreply.github.com>
2023-01-16 22:26:37 +0200

    Removed unneeded functions, still need to check if number exists

--- app/src/main/java/com/example/akelny/Users.java
+++ app/src/main/java/com/example/akelny/Users.java
@@ -29,15 +29,11 @@ public class Users {
             this.phoneNum = Phone;
         }
     }
-
-
-
     public Users() { // intialzing array
         users = new ArrayList<>();
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference();
     }
-
     public boolean validateNumber(String number) {
         boolean validated = false;
         String regex;
@@ -49,94 +45,14 @@ public class Users {
 
         return validated;
     }
-
-    public boolean validateEmail(String email) {
-        boolean validated = false;
-        String regex;
-        regex = "\\w*@\\w*.edu";
-        Pattern pt = Pattern.compile(regex);
-        Matcher mt = pt.matcher(email);
-        validated = mt.matches();
-
-        return validated;
-    }
-
-
-    public boolean ValidateUser(String name, String phone) //login
-    {
-        DatabaseReference users = database.getReference("User");
-        users.child(phone).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                Log.d("phone",phone);
-                if (task.isSuccessful()){
-
-                    if (task.getResult().exists()){
-                        Log.d("msg","First");
-
-                        validated = true;
-                    }else {
-                        Log.d("msg","Second");
-                        validated = false;
-                    }
-                }else {
-                    Log.d("msg","Third");
-                    validated = false;
-                }
-            }
-        });
-        Log.d("return",String.valueOf(validated));
-        return validated;
-    }
-
-
-    boolean verifyPassword(String password, String username) {
-
-        if (password.length() < 8) {
-            return false;
-        }
-        /*boolean hasLower = false, hasUpper = false, hasDigit = false, hasSpecial = false;
-        for (int i = 0; i < password.length(); i++) {
-            char c = password.charAt(i);
-            if (Character.isLowerCase(c)) {
-                hasLower = true;
-            } else if (Character.isUpperCase(c)) {
-                hasUpper = true;
-            } else if (Character.isDigit(c)) {
-                hasDigit = true;
-            } else if (c == '!' || c == '"' || c == '#' || c == '$' || c == '%' || c == '&' || c == '(' || c == ')' || c == '*' || c == '+' || c == ',' || c == '-' || c == '.' || c == '/' || c == ':' || c == ';' || c == '<' || c == '=' || c == '>' || c == '?' || c == '@' || c == '['  || c == ']' || c == '^' || c == '_' || c == '`' || c == '{' || c == '|' || c == '}' || c == '~') {
-                hasSpecial = true;
-            }
-        }
-        if (!hasLower || !hasUpper || !hasDigit || !hasSpecial) {
-            return false;
-        }
-        if (Character.isDigit(password.charAt(0)) || Character.isDigit(password.charAt(password.length() - 1))) {
-            return false;
-        }
-        if (password.equals(username) || new StringBuilder(password).reverse().toString().equals(username)) {
-            return false;
-        }*/
-        return true;
-    }
-
     public int register(String name, String phoneNum) {
-
-      /*  for (int i = 0; i < users.size(); i++) {
-            user temp;
-            temp = users.get(i);
-            if (temp.phoneNum.equals(phoneNum))
-                return 1;//phone number already exists
+        if(validateNumber(phoneNum)) {
+            user userToRegister = new user(name, phoneNum);
+            users.add(userToRegister);
+            myRef.child("User").child(phoneNum).setValue(userToRegister);
+            return 0;//successful registration
         }
-
-        if (!validateNumber(phoneNum)) {
-            return 2;//number is not valid (note, it should start with 0020)
-        }*/
-
-
-        user userToRegister = new user(name, phoneNum);
-        users.add(userToRegister);
-        myRef.child("User").child(phoneNum).setValue(userToRegister);
-        return 0;//successful registration
+        else
+            return 1;
     }
 }
