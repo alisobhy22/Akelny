60047652785c3411b9b08579f0faabde80a64405
mennawagdyy <mennawagdy@aucegypt.edu>
2023-01-19 13:52:31 +0200

    reservations file and profile page modifications

--- app/src/main/java/com/example/akelny/MainActivity.java
+++ app/src/main/java/com/example/akelny/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.akelny;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +12,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ListView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import android.util.Log;
+import com.google.android.gms.tasks.Task;
+
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     ImageButton profileBtn;
 
     public void alertDialog() {
-        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         String message;
         dialog.setTitle("This is the profile page");
         dialog.setPositiveButton("OK",
@@ -34,25 +46,27 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
-        AlertDialog alertDialog=dialog.create();
+        AlertDialog alertDialog = dialog.create();
         alertDialog.show();
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        listView= (ListView) findViewById(R.id.listview);
+        listView = (ListView) findViewById(R.id.listview);
         Adapter baseAdapter = new Adapter(getApplicationContext(), namesList, datesList, timesList, numberOfPeople);
         listView.setAdapter(baseAdapter);
+
 
         homeBtn = (ImageButton) findViewById(R.id.imageButton);
         profileBtn = (ImageButton) findViewById(R.id.imageButton2);
         homeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent= new Intent(MainActivity.this, Homepage.class);
+                Intent intent = new Intent(MainActivity.this, Homepage.class);
                 startActivity(intent);
             }
         });
@@ -61,6 +75,37 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 alertDialog();
+            }
+        });
+    }
+
+    public void getReservationsForAUser() {
+        ArrayList<Reservation> Reservations = new ArrayList<Reservation>();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference reservation = database.getReference("Reservations");
+        reservation.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (DataSnapshot child : task.getResult().getChildren()) {
+                        String numberOfPeople = child.child("numberOfPeople").getValue().toString();
+                        String reservationDate = child.child("reservationDate").getValue().toString();
+                        String reservationTime = child.child("reservationTime").getValue().toString();
+                        String specialRequests = child.child("specialRequests").getValue().toString();
+                        String order = child.child("order").getValue().toString();
+                        String status = child.child("status").getValue().toString();
+                        String userNumber = child.child("userNumber").getValue().toString();
+                        String restaurantName = child.child("restaurantName").getValue().toString();
+
+                        Reservation reservation = new Reservation(numberOfPeople, reservationDate, reservationTime, specialRequests, order, status, userNumber, restaurantName);
+
+                        if ((child.child("User Number").getValue().toString()) == "") {
+                            Reservations.add(reservation);
+                        }
+                    }
+                } else {
+                    Log.e("Reservation", "Error fetching data: " + task.getException().getMessage());
+                }
             }
         });
     }
--- app/src/main/java/com/example/akelny/Reservations.java
+++ app/src/main/java/com/example/akelny/Reservations.java
@@ -1,14 +1,111 @@
 package com.example.akelny;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Reservations extends AppCompatActivity {
+
+    //The appointment date has to be in this format "dd/MM/yyyy"
+    int checkIfDatePassed(String appointmentDateString) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        Date currentDate = new Date();
+        String currentDateString = formatter.format(currentDate);
+
+        try {
+            currentDate = formatter.parse(currentDateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Date appointmentDate = null;
+        try {
+            appointmentDate = formatter.parse(appointmentDateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return (currentDate.compareTo(appointmentDate));
+    }
+    int checkIfTimePassed(int appointmentHour, int appointmentMin, int appointmentSec) {
+        LocalTime currentTime, appointmentTime;
+        currentTime = java.time.LocalTime.now();
+        appointmentTime = LocalTime.of(appointmentHour, appointmentMin, appointmentSec);
+        return currentTime.compareTo(appointmentTime);
+    }
+
+    Boolean ifCanGiveFeedback(String appointmentDateString, int appointmentHour, int appointmentMin, int appointmentSec) {
+        int dateResult = checkIfDatePassed(appointmentDateString);
+        if (dateResult>0) //currentDate occurs after appointmentDate
+            return true; //user can give feedback
+        else if(dateResult<0) //currentDate occurs before appointmentDate
+            return false; //user can cancel the appointment
+        else //Both dates are equal, so comapare time
+        {
+            int timeResult = checkIfTimePassed(appointmentHour, appointmentMin, appointmentSec);
+            if (timeResult>0) //currentTime occurs after appointmentTime
+                return true; //user can give feedback
+            else if(timeResult<0) //currentTime occurs before appointmentTime
+                return false; //user can cancel the appointment
+            else //Both times are equal
+        }
+    }
+
+    Button giveFeedback, cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservations);
+
+        giveFeedback = findViewById(R.id.givefeedback);
+        cancel = findViewById(R.id.cancel);
+
+        boolean feedbackResult = ifCanGiveFeedback();
+
+        giveFeedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(feedbackResult)
+                {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Feedback Form");
+                    builder.setMessage("Please give us your feedback for the appointment.");
+                    builder.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                        }
+                    });
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+                }
+                else
+
+
+
+
+            }
+        });
     }
 }
+
+
+
+
